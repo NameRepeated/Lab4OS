22b7385e0256126cf65400d64c3c20ed550ff0a3
OE.Heart <leoouyixin@126.com>
2021-10-12 14:19:03 +0800

    Update

--- src/lab1/lib/print.c
+++ src/lab1/lib/print.c
@@ -1,10 +1,19 @@
 #include "print.h"
 #include "sbi.h"
+#include "types.h"
 
 void puts(char *s) {
-    // unimplemented
+    int i = 0;
+    while(s[i] != '\0') {
+        sbi_ecall(0x1, 0x0, (int)s[i++], 0, 0, 0, 0, 0);
+    }
 }
 
 void puti(int x) {
-    // unimplemented
+    int foo;
+    if (x > 0) {
+        foo = x%10;
+        puti(x/10);
+        sbi_ecall(0x1, 0x0, 0x30+foo, 0, 0, 0, 0, 0);
+    }
 }
