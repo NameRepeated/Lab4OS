01b8d48b0ca720eac75892dc06249046014ad1da
OE.Heart <leoouyixin@126.com>
2021-10-13 19:55:44 +0800

    Update Lab2 head.S _start

--- src/lab2/arch/riscv/kernel/sbi.c
+++ src/lab2/arch/riscv/kernel/sbi.c
@@ -26,22 +26,4 @@ struct sbiret sbi_ecall(int ext, int fid, uint64 arg0,
 	ret_val.value = a1;
 
 	return ret_val;
-    // struct sbiret ret_val;
-    // __asm__ volatile (
-    //     "mv x17, %[ext]\n"
-    //     "mv x16, %[fid]\n"
-	// 	"mv x10, %[arg0]\n"
-	// 	"mv x11, %[arg1]\n"
-	// 	"mv x12, %[arg2]\n"
-	// 	"mv x13, %[arg3]\n"
-	// 	"mv x14, %[arg4]\n"
-	// 	"mv x15, %[arg5]\n"
-	// 	"ecall\n"
-    //     "mv %[ret_val], x10\n"
-	// 	"mv %[ret_val]+8, x11\n"
-    //     : [ret_val] "=r" (ret_val)
-    //     : [ext] "r" (ext), [fid] "r" (fid), [arg0] "r" (arg0), [arg1] "r" (arg1), [arg2] "r" (arg2), [arg3] "r" (arg3), [arg4] "r" (arg4), [arg5] "r" (arg5)
-    //     : "memory"
-    // );
-    // return ret_val;
 }
--- src/lab2/init/main.c
+++ src/lab2/init/main.c
@@ -1,11 +1,11 @@
-#include "print.h"
+#include "printk.h"
 #include "sbi.h"
 
 extern void test();
 
 int start_kernel() {
-    puti(2021);
-    puts(" Hello RISC-V\n");
+    // printk(2021);
+    printk(" Hello RISC-V\n");
 
     test(); // DO NOT DELETE !!!
 
