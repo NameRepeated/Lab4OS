93b6aa62c342c6e0e2e725f99e61968dba592792
OE.Heart <leoouyixin@126.com>
2021-10-27 17:17:25 +0800

    Fix typo in lab1

--- src/lab1/lib/print.c
+++ src/lab1/lib/print.c
@@ -5,7 +5,7 @@
 void puts(char *s) {
     int i = 0;
     while(s[i] != '\0') {
-        sbi_ecall(0x1, 0x0, (int)s[i++], 0, 0, 0, 0, 0);
+        sbi_ecall(SBI_PUTCHAR, 0x0, (int)s[i++], 0, 0, 0, 0, 0);
     }
 }
 
@@ -14,6 +14,6 @@ void puti(int x) {
     if (x > 0) {
         foo = x%10;
         puti(x/10);
-        sbi_ecall(0x1, 0x0, 0x30+foo, 0, 0, 0, 0, 0);
+        sbi_ecall(SBI_PUTCHAR, 0x0, 0x30+foo, 0, 0, 0, 0, 0);
     }
 }
